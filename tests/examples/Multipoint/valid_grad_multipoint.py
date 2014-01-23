2e89c47986742be47e8d413661fd053068969f81
Matthieu Meaux <matthieu.meaux@eads.net>
2014-01-23 15:18:49 +0100

    Test case multi-point

---
+++ tests/examples/Multipoint/valid_grad_multipoint.py
@@ -0,0 +1,81 @@
+from OpenDACE.ValidGrad.FDValidGrad import FDValidGrad
+from DLLM.DLLMEval.DLLMWrapper import DLLMWrapper
+
+config_dict={}
+# Operating condition configuration
+config_dict['cond1.OC.Mach']=0.8
+config_dict['cond1.OC.AoA']=3.5
+config_dict['cond1.OC.altitude']=10000.
+
+# Parameterisation configuration
+config_dict['cond1.param.geom_type']='Broken'
+config_dict['cond1.param.n_sect']=20
+config_dict['cond1.param.airfoil.type']='simple'
+config_dict['cond1.param.airfoil.AoA0']=-2.
+config_dict['cond1.param.airfoil.Cm0']=-0.1
+# config_dict['cond1.param.airfoil.type']='meta'
+# config_dict['cond1.param.airfoil.surrogate_model']='../MetaModelCleaning.xml'
+config_dict['cond1.param.desc.span.type']='DesignVariable'
+config_dict['cond1.param.desc.span.value']=34.1
+config_dict['cond1.param.desc.span.bounds']=(10.,50.)
+config_dict['cond1.param.desc.sweep.type']='DesignVariable'
+config_dict['cond1.param.desc.sweep.value']=34.
+config_dict['cond1.param.desc.sweep.bounds']=(0.,40.)
+config_dict['cond1.param.desc.break_percent.type']='Variable'
+config_dict['cond1.param.desc.break_percent.value']=33.
+#config_dict['cond1.param.desc.break_percent.bounds']=(20.,40.)
+config_dict['cond1.param.desc.root_chord.type']='DesignVariable'
+config_dict['cond1.param.desc.root_chord.value']=6.1
+config_dict['cond1.param.desc.root_chord.bounds']=(5.,7.)
+config_dict['cond1.param.desc.break_chord.type']='DesignVariable'
+config_dict['cond1.param.desc.break_chord.value']=4.6
+config_dict['cond1.param.desc.break_chord.bounds']=(3.,5.)
+config_dict['cond1.param.desc.tip_chord.type']='DesignVariable'
+config_dict['cond1.param.desc.tip_chord.value']=1.5
+config_dict['cond1.param.desc.tip_chord.bounds']=(1.,2.)
+config_dict['cond1.param.desc.root_height.type']='DesignVariable'
+config_dict['cond1.param.desc.root_height.value']=1.28
+config_dict['cond1.param.desc.root_height.bounds']=(1.,1.5)
+config_dict['cond1.param.desc.break_height.type']='DesignVariable'
+config_dict['cond1.param.desc.break_height.value']=0.97
+config_dict['cond1.param.desc.break_height.bounds']=(0.8,1.2)
+config_dict['cond1.param.desc.tip_height.type']='DesignVariable'
+config_dict['cond1.param.desc.tip_height.value']=0.33
+config_dict['cond1.param.desc.tip_height.bounds']=(0.2,0.5)
+
+# DLLM configuration
+config_dict['cond1.DLLM.type']='Solver'
+config_dict['cond1.DLLM.method']='inhouse'
+config_dict['cond1.DLLM.relax_factor']=0.99
+config_dict['cond1.DLLM.stop_residual']=1e-9
+config_dict['cond1.DLLM.max_iterations']=100
+config_dict['cond1.DLLM.gamma_file_name']='gamma.dat'
+#config_dict['cond1.DLLM.F_list_names']=['Lift','Drag','Drag_Pressure','Drag_Friction','Cl', 'Cd', 'Cdp', 'Cdf', 'LoD']
+#config_dict['cond1.DLLM.F_list_names']=['Lift','Drag','Drag_Pressure','Drag_Friction','LoD']
+#config_dict['cond1.DLLM.target_Cl']=0.5
+#config_dict['cond1.DLLM.target_Lift']=769200.
+
+DLLMcond1=DLLMWrapper('cond1')
+DLLMcond1.configure(config_dict)
+DLLMcond1.set_out_format('numpy')
+DLLMcond1.set_grad_format('numpy')
+
+x0=DLLMcond1.get_x0()
+print 'dv array shape',x0.shape
+print 'dv_array=',x0
+
+val_grad=FDValidGrad(2,DLLMcond1.run,DLLMcond1.run_grad,fd_step=1.e-8)
+ok,df_fd,df=val_grad.compare(x0,treshold=1.e-6,split_out=True,return_all=True)
+
+for j in xrange(len(df[:,0])):
+    fid=open('gradient_file'+str(j)+'.dat','w')
+    for i in xrange(len(x0)):
+        fid.write(str(i)+' '+str(df_fd[j,i])+' '+str(df[j,i])+'\n')
+    fid.close()
+
+print '\n****************************************************'
+if ok:
+    print 'DLLMWrapper gradients are valid.'
+else:
+    print 'DLLMWrapper gradients are not valid!'
+print '****************************************************'
