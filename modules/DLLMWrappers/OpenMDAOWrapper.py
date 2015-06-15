DLLM Wrapper : 	Almost ok, still a bug in the gradient.

modules/DLLMWrappers/OpenMDAOWrapper.py
@@ -45,10 +45,9 @@ class DLLMOpenMDAOComponent(Component):
     tip_height = Float(desc='tip_height', default_value=0.33, iotype="in")
     # Operating conditions variables
     Mach = Float(iotype='in', default_value=0.7, desc='Mach')
-    altitude = Float(iotype='in', default_value=10000., desc='altitude')
-    T0 = Float(iotype='in', default_value=15., desc='T0')
-    P0 = Float(iotype='in', default_value=101325., desc='P0')
-    humidity = Float(iotype='in', default_value=0.0, desc='humidity')
+    altitude = Float(iotype='in', default_value=0., desc='Altitude')
+    T0 = Float(iotype='in', default_value=OperatingCondition.T0, desc='Ground ISA ref Temperature')
+    P0 = Float(iotype='in', default_value=OperatingCondition.P0, desc='Ground ISA ref Pressure')
 
     def __init__(self, N, verbose=0):
         """Initialization of DLLM component.
@@ -58,13 +57,14 @@ class DLLMOpenMDAOComponent(Component):
             - verbose : integer : verbosity level
         """
         self.N = N
-        self.OC = self.__set_OC(OC_name="LLW_OC")
+        self.OC = None
         self.rtwist = np.zeros(N)
-        super(DLLMOpenMDAOComponent, self).__init__()
         self.__display_wing_param = True
         self.__verbose = verbose
+        self.wing_param = None
+        super(DLLMOpenMDAOComponent, self).__init__()
+        self.__set_OC(OC_name="LLW_OC")
         self.__set_wing_param()
-
         self.DLLM = DLLMTargetLift('test', self.wing_param,
                                    self.OC, verbose=self.__verbose)
         self.DLLM.run_direct()
@@ -108,14 +108,13 @@ class DLLMOpenMDAOComponent(Component):
 
     def __set_OC(self, OC_name='OC1'):
         """ Set default operating conditions"""
-        OC = OperatingCondition(OC_name, atmospheric_model='ISA')
-        OC.set_Mach(self.Mach)
-        OC.set_altitude(self.altitude)
-        OC.set_T0_deg(self.T0)
-        OC.set_P0(self.P0)
-        OC.set_humidity(self.humidity)
-        OC.compute_atmosphere()
-        return OC
+        self.OC = OperatingCondition(OC_name, atmospheric_model='ISA')
+        self.OC.set_Mach(self.Mach)
+        self.OC.set_altitude(self.altitude)
+        self.OC.set_T0(self.T0)
+        self.OC.set_P0(self.P0)
+        self.OC.set_humidity(1.)
+        self.OC.compute_atmosphere()
 
     def execute(self):
         """ Perform a DLLM computation with the """
@@ -126,8 +125,8 @@ class DLLMOpenMDAOComponent(Component):
             else:
                 exec("self.wing_param.set_value('%s',float(self.%s))" %
                      (dv_id, dv_id))
-
         self.wing_param.update()
+        self.__set_OC()
         self.DLLM.run_direct()
         self.DLLM.run_post()
         output = self.DLLM.get_F_list()
@@ -149,21 +148,22 @@ class DLLMOpenMDAOComponent(Component):
 
     def provideJ(self):
         """Calculate the Jacobian according inputs and outputs"""
-        return self.DLLM.get_dpF_list_dpchi()
+        self.DLLM.run_adjoint()
+        return np.array(self.DLLM.get_dF_list_dchi())
 
-if __name__ == "__main__":
-    import time
-    tt = time.time()
-    N = 20
-    DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=1)
-    DLLMOpenMDAO.OC.set_altitude(190000.)
-    DLLMOpenMDAO.DLLM.set_target_Lift(606570.049598)
-    DLLMOpenMDAO.run()
-
-    print "Final point :"
-    print "    -lift :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Lift()
-    print "    -total drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag()
-    print "    -induced drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Induced()
-    print "    -wave drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Pressure()
-    print "    -wave drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Wave()
-    print "    -friction drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Friction()
+# if __name__ == "__main__":
+#     import time
+#     tt = time.time()
+#     N = 20
+#     DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=1)
+#     DLLMOpenMDAO.OC.set_altitude(190000.)
+#     DLLMOpenMDAO.DLLM.set_target_Lift(606570.049598)
+#     DLLMOpenMDAO.run()
+# 
+#     print "Final point :"
+#     print "    -lift :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Lift()
+#     print "    -total drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag()
+#     print "    -induced drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Induced()
+#     print "    -wave drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Pressure()
+#     print "    -wave drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Wave()
+#     print "    -friction drag :", DLLMOpenMDAO.DLLM.get_DLLMPost().get_Drag_Friction()

tests/DLLM_wrapper_tests/test_openmdao_wrapper.py
@@ -4,36 +4,43 @@ Created on Jun 4, 2015
 @author: Francois
 '''
 
-import unittest
 from DLLMWrappers.OpenMDAOWrapper import DLLMOpenMDAOComponent
 from DLLM.DLLMKernel.DLLMPost import DLLMPost
+from MDOTools.ValidGrad.FDValidGrad import FDValidGrad
+from MDOTools.OC.operating_condition import OperatingCondition
+import unittest
+import numpy as np
 
 class TestDLLM_wrapper(unittest.TestCase):
     
-     
+      
     def test_init_component(self):
-        N = 5
-        DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=0)
+        DLLMOpenMDAO = DLLMOpenMDAOComponent(N=5, verbose=0)
         default_lift = 0.00
-        self.assertAlmostEqual(DLLMOpenMDAO.Lift, default_lift, places=6)
-  
+        self.assertEqual(DLLMOpenMDAO.Lift, default_lift)
+   
     def test_change_OC(self):
-        N = 5
-        DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=0)
-        default_altitude = 10000.
-        self.assertAlmostEqual(default_altitude, DLLMOpenMDAO.OC.get_altitude(), places=6)
-        new_altitude = 11000.
-        DLLMOpenMDAO.OC.set_altitude(11000.)
-        self.assertAlmostEqual(new_altitude, DLLMOpenMDAO.OC.get_altitude(), places=6)
- 
+        DLLMOpenMDAO = DLLMOpenMDAOComponent(N = 5, verbose=0)
+        self.assertEqual(0., DLLMOpenMDAO.OC.get_altitude())
+        DLLMOpenMDAO.altitude=0.
+        DLLMOpenMDAO.Mach = 0.
+        DLLMOpenMDAO.T0 = OperatingCondition.T0+10
+        DLLMOpenMDAO.P0 = OperatingCondition.P0+10
+        
+        DLLMOpenMDAO.execute()
+        self.assertEqual(0., DLLMOpenMDAO.OC.get_altitude())
+        self.assertEqual(0., DLLMOpenMDAO.OC.get_Mach())
+        self.assertEqual(OperatingCondition.T0+10, DLLMOpenMDAO.OC.get_T0())
+        self.assertEqual(OperatingCondition.P0+10, DLLMOpenMDAO.OC.get_P0())
+  
     def test_change_dv(self):
         N = 5
         DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=0)
         new_span = 48.0
-        DLLMOpenMDAO.wing_param.set_value('span', new_span)
-        DLLMOpenMDAO.wing_param.update()
-        self.assertAlmostEqual(new_span, DLLMOpenMDAO.wing_param.get_span())
- 
+        DLLMOpenMDAO.span= new_span
+        DLLMOpenMDAO.execute()
+        self.assertEqual(new_span, DLLMOpenMDAO.wing_param.get_span())
+  
     def test_exec(self):
         N = 5
         DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=0)
@@ -42,7 +49,7 @@ class TestDLLM_wrapper(unittest.TestCase):
         DLLMOpenMDAO.execute()
 #         print "Final lift:",DLLMOpenMDAO.Lift
         self.assertAlmostEqual(Target_Lift, DLLMOpenMDAO.Lift,places=3)
-         
+          
     def test_list_deriv_vars(self):
         N = 5
         DLLMOpenMDAO = DLLMOpenMDAOComponent(N, verbose=0)
@@ -68,7 +75,58 @@ class TestDLLM_wrapper(unittest.TestCase):
         n_dv = len(DLLMOpenMDAO.wing_param.get_dv_id_list())
         self.assertEqual(n_dv, Jacobian.shape[0])
         self.assertEqual(n_F, Jacobian.shape[1])
-        DLLMOpenMDAO.check_gradient()
+        
+        def update_dllm_from_x(x):
+            DLLMOpenMDAO.rtwist = x[:5]
+            DLLMOpenMDAO.span = x[5]
+            DLLMOpenMDAO.sweep = x[6]
+            DLLMOpenMDAO.break_percent = x[7]
+            DLLMOpenMDAO.root_chord = x[8]
+            DLLMOpenMDAO.break_chord = x[9]
+            DLLMOpenMDAO.tip_chord = x[10]
+            DLLMOpenMDAO.root_height = x[11]
+            DLLMOpenMDAO.break_height = x[12]
+            DLLMOpenMDAO.tip_height=x[13]
+            assert(len(x)==14)
+        
+        def d_obj(x):
+            update_dllm_from_x(x)
+            DLLMOpenMDAO.execute()
+            return DLLMOpenMDAO.provideJ()
+            
+        def obj(x):
+            update_dllm_from_x(x)
+            DLLMOpenMDAO.execute()
+            return DLLMOpenMDAO.DLLM.get_F_list()
+        
+        treshold=1e-4
+        val_grad=FDValidGrad(1,obj,d_obj,fd_step=1.e-7)
+        x0=DLLMOpenMDAO.wing_param.get_dv_array()
+        ok,df_fd,df=val_grad.compare(x0,treshold=treshold,split_out=True,return_all=True, iprint =False)
+        varnames, funcnames=DLLMOpenMDAO.list_deriv_vars()
+        varnames=np.concatenate((['tw0','tw1','tw2','tw3','tw4'],np.array(varnames[1:])))
+        funcnames=np.array(funcnames)
+        k=0
+        ok = True
+        for fd, gr in zip(df_fd,df):
+            err=np.where(((fd-gr)/fd>treshold) & (fd>1e-5))[0]
+            if err.size>0:
+                #Compute absolute error when gradient or fd is lower than threshold
+                inf_tol=err[np.where((np.abs(gr[err])<treshold) | (np.abs(fd[err])<treshold))[0]]
+                #Scale gradient by function value for the lift issue in particular
+                f_val=getattr(DLLMOpenMDAO,funcnames[k])
+                actually_ok=np.where(((fd[inf_tol]-gr[inf_tol])<treshold*f_val))[0]
+                real_err=np.delete(err,actually_ok)
+                if real_err.size >0:
+                    print "Errror in d",funcnames[k]," /d ",varnames[real_err]
+                    print "df analytic = ",gr[real_err]
+                    print "df finite d = ",fd[real_err]
+                    ok=False
+            k=k+1
+        assert(ok)
+        #This is not that reliable... threshold is hard coded at 1e-5, only relative error is computed...
+        #out = DLLMOpenMDAO.check_gradient(fd_form='central', fd_step=1e-5, fd_step_type='relative')
+        #assert(len(out[-1])==0)
         
 
 